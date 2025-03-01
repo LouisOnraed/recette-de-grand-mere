1310e1d9edeb96cede4e5c29d6fea349f348867a
LouisOnraed <115577207+LouisOnraed@users.noreply.github.com>
2025-03-01 16:30:50 +0000

    update of the API call

--- src/script/js/weather.js
+++ src/script/js/weather.js
@@ -1,8 +1,8 @@
 const apiKey = "aaa70a90a605a14bf4ff3af2d4d6bc39";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=41.89&lon=12.49&appid=aaa70a90a605a14bf4ff3af2d4d6bc39";
 
 async function weather() {
-    const response = await fetch(apiURL + "rome" + `&appid=${apiKey}`);
+    const response = await fetch(apiURL);
     if (response.ok == false) {
         console.log('Error Weather');
     }
